3f0172839e2046b6e4f65fda3514f595c08114fc
Marochka77 <43371228+Marochka77@users.noreply.github.com>
2020-07-31 15:15:07 +0300

    Add files via upload
    This is the right file, the source file

---
+++ Task.java
@@ -0,0 +1,19 @@
+package ua.karazin.rbecsfirst.ma;
+import java.util.Scanner; 
+
+public class Task {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input integer number i:");
+		int i = scan.nextInt();
+		
+		System.out.println("Input real number x:");
+		double x = scan.nextDouble(); 
+		
+		System.out.printf("x = %.2f;  i = %d%n",x,i);
+		System.out.printf("x + i = %.2f%n",(x+i));
+		scan.close();
+	}
+}
